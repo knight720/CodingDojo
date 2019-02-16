70900f0bfed5b922a000cae836ca97d5dceb6b2f
Knight Huang <ninku720@gmail.com>
2019-02-16 16:15:54 +0800

    Firt Passed

--- CodingDojo/PokerHandsTest.cs
+++ CodingDojo/PokerHandsTest.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace CodingDojo
 {
     [TestClass]
-    public class UnitTest1
+    public class PokerHandsTest
     {
         [TestMethod]
         public void TestMethod1()
